9239eb0ea9dc1a185ee3f9d95111bc30a9f73ffe
Hani Q <haniq.eth@cloudlynx.dev>
2022-04-08 23:53:28 +0300

    Boilerplate
    Signed-off-by: Hani Q <haniq.eth@cloudlynx.dev>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,51 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// pages/_app.tsx
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import * as React from 'react';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../styles/theme/theme';
+import createEmotionCache from '../src/utility/createEmotionCache';
+import { useEffect, useState } from 'react';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp
+export default function MyApp(props: MyAppProps) {
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+
+  if (typeof window === 'undefined') {
+    return <></>;
+  } else {
+    const {
+      Component,
+      emotionCache = clientSideEmotionCache,
+      pageProps
+    } = props;
+    return (
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    );
+  }
+}
